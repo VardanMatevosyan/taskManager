10aed9e574a74f8c705f3e928d03814309243a8d
VardanMatevosyan <vardanmt@gmail.com>
2019-08-04 17:06:19 +0300

    added date format using pipe

---
+++ frontend/src/main/web/src/app/components/utils/date-format/dateFormat.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Pipe({
+    name: 'dateFormatPipe',
+})
+
+export class DateFormat implements PipeTransform  {
+  transform(value: string) {
+     var datePipe = new DatePipe("en-US");
+     value = datePipe.transform(value, 'y-MMM-d hh:mm a');
+     return value;
+   }
+}
